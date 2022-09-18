a881ff724c92c1a3364d9af0964ead2408b591f7
lyl1710 <phanlong1017@gmail.com>
2022-09-18 11:28:47 +0700

    long

--- index.js
+++ index.js
@@ -1,34 +1,76 @@
-var btnDecrementValue=document.getElementsByClassName("btn-decrementValue")
-var btnIncrementValue=document.getElementsByClassName("btn-incrementValue")
+const btnDecrementValue = document.getElementsByClassName("btn-decrement-quantity")
+const btnIncrementValue = document.getElementsByClassName("btn-increment-quantity")
+const oderQuantity = document.getElementById('oder-quantity')
+const productCost = document.getElementById('product-cost')
+const cost = parseInt(productCost.value);
+const sale = 0;
+const customerPayInput = document.getElementById('customer-pay');
+const needToPay = document.getElementById('need-to-pay');
+const tranferMoney = document.querySelector('.suggest-money');
+const cashMoney = document.querySelector('.tranfer-money');
+const moneyRadio1 = document.getElementById('cash');
+const moneyRadio2 = document.getElementById('transfer');
 
 
+btnIncrementValue[0].addEventListener('click', function (event) {
+  var oldQuantity = parseInt(oderQuantity.value);
+  var newQuantity = oldQuantity + 1;
+  oderQuantity.value = newQuantity;
+  var newCost = cost * newQuantity;
+  productCost.value = newCost;
+  document.getElementById('total-order').innerHTML = newQuantity;
+  document.getElementById('total-money').innerHTML = newCost;
+  document.getElementById('need-to-pay').innerHTML = newCost - sale;
+});
 
-for (var i=0;i<btnIncrementValue.length; i++) {
 
-    btnIncrementValue[i].addEventListener('click',function(event){
-          var buttonClick = event.target;
-  var input=buttonClick.parentElement.children[1];
-  var inputValue=input.value;
+btnDecrementValue[0].addEventListener('click', function (event) {
+  var oldQuantity = parseInt(oderQuantity.value);
+  if (oldQuantity == 0) {
+    oderQuantity.value = 0;
+    productCost.value = 0;
+  } else {
+    var newQuantity = oldQuantity - 1;
+    oderQuantity.value = newQuantity;
+    var newCost = cost * newQuantity;
+    productCost.value = newCost;
+    document.getElementById('total-order').innerHTML = newQuantity;
+    document.getElementById('total-money').innerHTML = newCost;
+    document.getElementById('need-to-pay').innerHTML = newCost - sale;
+  }
+});
 
-  var newValue=parseInt(inputValue)+1;
-input.value=newValue
-        });
+customerPayInput.addEventListener('keyup', function (event) {
+  var need = parseInt(needToPay.textContent);
+  var pay = parseInt(customerPayInput.value);
+  var excess = pay - need;
+  document.getElementById('excess-money').innerHTML = excess;
+});
 
+document.querySelectorAll('.btn-suggest').forEach(item => {
+  item.addEventListener('click', event => {
+    customerPayInput.value = item.textContent;
+    var need = parseInt(needToPay.textContent);
+    var pay = parseInt(customerPayInput.value);
+    var excess = pay - need;
+    document.getElementById('excess-money').innerHTML = excess;
+  })
+})
+
+
+function valueChange(event){
+  if(moneyRadio1.checked){
+    
+    tranferMoney.style.display = "block";
+    cashMoney.style.display = "none";
+
+  }else{
+    tranferMoney.style.display = "none";
+    cashMoney.style.display = "block";
+  }
 }
 
+moneyRadio1.addEventListener('change', valueChange);
 
-for (var i=0;i<btnDecrementValue.length; i++) {
+moneyRadio2.addEventListener('change', valueChange);
 
-    btnDecrementValue[i].addEventListener('click',function(event){
-        var buttonClick = event.target;
-        var input=buttonClick.parentElement.children[1];
-        var inputValue=input.value;
-        if(inputValue==0){
-            input.value=0;
-            return
-          }
-        var newValue=parseInt(inputValue)-1;
-      input.value=newValue
-    });
-
-}
